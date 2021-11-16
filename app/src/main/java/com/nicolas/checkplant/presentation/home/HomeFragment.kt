0ddbfcceb73264befc64026aa3370137083ccee8
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-11-16 13:08:23 -0300

    init commit

---
+++ app/src/main/java/com/nicolas/checkplant/presentation/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package com.nicolas.checkplant.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nicolas.checkplant.R
+
+class HomeFragment : Fragment() {
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+}
