ffbda77e1f3ff254a9d0e5dc8b1f4da313cf16b6
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-11-18 17:57:17 -0300

    utils

---
+++ app/src/main/java/com/nicolas/checkplant/common/AdapterPlant.kt
@@ -0,0 +1,64 @@
+package com.nicolas.checkplant.common
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.nicolas.checkplant.databinding.LayoutItemsPlantBinding
+import com.nicolas.checkplant.domain.model.Plant
+
+class AdapterPlant(
+    private val listPlants: List<Plant>,
+    private val clickPlant: ((plant: Plant) -> Unit)
+) : RecyclerView.Adapter<AdapterPlant.MainViewHolder>() {
+
+    class MainViewHolder(
+        binding: LayoutItemsPlantBinding,
+        private val clickPlant: ((plant: Plant) -> Unit)
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        private val title = binding.tvNamePlant
+        private val description = binding.tvDescriptionPlant
+        private val image = binding.imgPlant
+        private val day = binding.tvDayPlant
+
+        fun bind(plant: Plant) {
+
+            title.text = plant.name
+            description.text = plant.description
+            day.text = plant.day
+
+            loadImagePlant(image, plant.image)
+
+            itemView.setOnClickListener {
+                clickPlant.invoke(plant)
+            }
+        }
+
+        private fun loadImagePlant(imageView: ImageView, uriImage: String) {
+            uriImage.let {
+                Glide.with(imageView.context)
+                    .load(uriImage)
+                    .centerCrop()
+                    .into(imageView)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val layout =
+            LayoutItemsPlantBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        return MainViewHolder(layout, clickPlant)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        holder.bind(listPlants[position])
+    }
+
+    override fun getItemCount() = listPlants.size
+}
---
+++ app/src/main/java/com/nicolas/checkplant/common/ViewUtils.kt
@@ -0,0 +1,14 @@
+package com.nicolas.checkplant.common
+
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+fun View.snack(message: String, duration: Int = Snackbar.LENGTH_SHORT) {
+    Snackbar.make(this, message, duration).show()
+}
+
+fun Toast.showToast(msg: String, context: Context, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(context, msg, duration).show()
+}
