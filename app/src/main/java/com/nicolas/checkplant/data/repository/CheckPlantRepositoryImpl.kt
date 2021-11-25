349be3edc19ea970ab7f4814a4d817f9cd01d5ac
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-11-25 17:11:40 -0300

    main repository

---
+++ app/src/main/java/com/nicolas/checkplant/data/repository/CheckPlantRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.nicolas.checkplant.data.repository
+
+import com.nicolas.checkplant.data.data_source.local.LocalDataSource
+import com.nicolas.checkplant.domain.model.Plant
+import com.nicolas.checkplant.domain.repository.CheckPlantRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CheckPlantRepositoryImpl @Inject constructor(
+    private val localDataSource: LocalDataSource
+) : CheckPlantRepository {
+
+    override fun getPlants(): Flow<List<Plant>> {
+        return localDataSource.getPlants()
+    }
+
+    override suspend fun getPlantById(id: Int): Plant {
+        return localDataSource.getPlantById(id)
+    }
+
+    override suspend fun insertPlant(plant: Plant) {
+        localDataSource.insertPlant(plant)
+    }
+
+    override suspend fun deletePlant(plant: Plant) {
+        localDataSource.deletePlant(plant)
+    }
+}
---
+++ app/src/main/java/com/nicolas/checkplant/domain/repository/CheckPlantRepository.kt
@@ -0,0 +1,16 @@
+package com.nicolas.checkplant.domain.repository
+
+import com.nicolas.checkplant.domain.model.Plant
+import kotlinx.coroutines.flow.Flow
+
+interface CheckPlantRepository {
+
+    fun getPlants(): Flow<List<Plant>>
+
+    suspend fun getPlantById(id: Int): Plant
+
+    suspend fun insertPlant(plant: Plant)
+
+    suspend fun deletePlant(plant: Plant)
+
+}
