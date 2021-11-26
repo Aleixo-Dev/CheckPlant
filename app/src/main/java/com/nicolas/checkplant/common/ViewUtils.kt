1f7119bb3ab451b70fdc4cb9e5f0cdb948d83718
Aleixo-Dev <nicolasaleixo2020@gmail.com>
2021-11-26 16:53:13 -0300

    fix utils

--- app/src/main/java/com/nicolas/checkplant/common/ViewUtils.kt
+++ app/src/main/java/com/nicolas/checkplant/common/ViewUtils.kt
@@ -3,12 +3,13 @@ package com.nicolas.checkplant.common
 import android.content.Context
 import android.view.View
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
 fun View.snack(message: String, duration: Int = Snackbar.LENGTH_SHORT) {
     Snackbar.make(this, message, duration).show()
 }
 
-fun Toast.showToast(msg: String, context: Context, duration: Int = Toast.LENGTH_SHORT) {
+fun Fragment.showToast(msg: String, context: Context, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(context, msg, duration).show()
 }
